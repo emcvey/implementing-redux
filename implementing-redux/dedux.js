0504740a318c3d815ea6ea5670a89bbed948375e
Erika McVey <erikammcvey@gmail.com>
2021-06-06 18:56:45 -0400

    initial store and getState

--- implementing-redux/dedux.js
+++ implementing-redux/dedux.js
@@ -4,9 +4,20 @@ export default {
 }
 
 function createStore(reducerFn) {
+  let currentState = reducerFn()
   if (typeof reducerFn !== 'function') {
     throw Error('The reducer passed must be a function.')
   }
+
+  function getState() {
+    return currentState
+  }
+
+  const store = {
+    getState,
+  }
+
+  return store
 }
 
 function applyMiddleware() {}
