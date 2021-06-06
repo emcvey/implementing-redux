185ca0d1ec654891af9a12a131d6c7b9bedc2954
Erika McVey <erikammcvey@gmail.com>
2021-06-06 19:17:56 -0400

    dispatch functionality

--- implementing-redux/dedux.js
+++ implementing-redux/dedux.js
@@ -5,8 +5,23 @@ export default {
 
 function createStore(reducerFn) {
   let currentState = reducerFn()
+  let reducer = reducerFn
   if (typeof reducerFn !== 'function') {
     throw Error('The reducer passed must be a function.')
+  }
+
+  function dispatch(action) {
+    if (
+      !action ||
+      typeof action !== 'object' ||
+      typeof action.type !== 'string'
+    ) {
+      throw Error(
+        'The dispatched action must be an object with type property that has a string value: { type: string, ...any }'
+      )
+    }
+
+    currentState = reducer(currentState, action)
   }
 
   function getState() {
@@ -14,6 +29,7 @@ function createStore(reducerFn) {
   }
 
   const store = {
+    dispatch,
     getState,
   }
 
